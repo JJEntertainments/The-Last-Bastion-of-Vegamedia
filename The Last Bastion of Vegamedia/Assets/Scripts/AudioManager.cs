ae894c2dbeabc5e242bfad737803dc452e4a0af4
JoseMiguelLoa <josemiguel.lopez.asensio@ces-vegamedia.es>
2024-05-07 13:22:45 +0200

    Musica añadida al proyecto

---
+++ The Last Bastion of Vegamedia/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager instance;
+
+    public static AudioManager Instance
+    {
+        get { return instance; }
+    }
+
+    public AudioSource musicSource;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Método para reproducir música
+    public void PlayMusic(AudioClip music)
+    {
+        musicSource.clip = music;
+        musicSource.Play();
+    }
+}
